e77e772db69eebe20f15b861c9ae6c4dffabe622
AndreikaMarmazetka <37284968+AndreikaMarmazetka@users.noreply.github.com>
2018-04-13 09:41:42 +0400

    Initial commit

---
+++ src/main/java/HangredDoors.java
@@ -0,0 +1,19 @@
+/**
+ * Created by apopov on 10.04.2018.
+ */
+public class HangredDoors {
+        public static void main(String[] args) {
+                 boolean[] doors = new boolean[101];
+                for (int i = 1; i < doors.length; i++) {
+                      for (int j = i; j < doors.length; j += i) {
+                              doors[j] = !doors[j];
+                      }
+                }
+
+                for (int i = 1; i < doors.length; i++) {
+                         if (doors[i]){
+                              System.out.printf("Door %d is open.%n", i);
+                         }
+                }
+        }
+}
